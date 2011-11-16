2c2001f5113d1cc46348b2f4efd3714ab2b009a4
Deepak CVR <deepak.cvr@gmail.com>
2011-11-16 14:34:27 +0530

    password regex

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -16,7 +16,7 @@ class ClientsController < ApplicationController
   	@client = Client.new(params[:client])
   	if @client.save
      sign_in @client
-     flash[:success] = "A message will be sent to your email account for verification. Please store your password in a safe place; you are responsible for ensuring that it is not shared with anyone."
+     flash[:success] = "Sign up successful! A message will be sent to your email account for verification. Please store your password in a safe place; you are responsible for ensuring that it is not shared with anyone."
   	 redirect_to signin_path 
     else
       render :action => "new"
